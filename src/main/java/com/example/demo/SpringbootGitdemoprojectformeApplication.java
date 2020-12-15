bb10a3acc060a81848a4b0c816ac333ce0d97d35
abhisheksharma <abhishesk.s@flitwebs.com>
2020-12-15 16:16:53 +0530

    First commit

---
+++ src/main/java/com/example/demo/SpringbootGitdemoprojectformeApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitdemoprojectformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitdemoprojectformeApplication.class, args);
+		
+		System.out.println("This project is upload on abhishek");
+	}
+
+}
